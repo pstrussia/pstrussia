d2a6bd1d7b3f2d13ef426fb4f3a4c5339408e113
gvozdevdanil <gvozdev-danil@mail.ru>
2022-09-14 23:47:48 +0300

    feat(t:none) - fix default sort

--- local/templates/divasoft/components/bitrix/catalog.section/main/result_modifier.php
+++ local/templates/divasoft/components/bitrix/catalog.section/main/result_modifier.php
@@ -1,28 +1,32 @@
-<?if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+<?
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
+
 use Bitrix\Main\Type\Collection;
 use Bitrix\Currency\CurrencyTable;
 use Bitrix\Iblock;
 ?>
 
-<?global $PHOENIX_TEMPLATE_ARRAY;
+<?
+
+global $PHOENIX_TEMPLATE_ARRAY;
 
 CPhoenixSku::getHIBlockOptions();
 
-if(intval($arResult["ID"]))
-{
+if (intval($arResult["ID"])) {
     $arSelect = Array("ID", "UF_*");
-    $arFilter = Array('IBLOCK_ID'=>$arParams["IBLOCK_ID"], "ID" => $arResult["ID"]);
+    $arFilter = Array('IBLOCK_ID' => $arParams["IBLOCK_ID"], "ID" => $arResult["ID"]);
     $db_list = CIBlockSection::GetList(Array(), $arFilter, false, $arSelect);
     $ar_result = $db_list->GetNext();
 
-    if(strlen($ar_result["UF_PHX_CTLG_TXT_P"]) > 0)
-    {
+    if (strlen($ar_result["UF_PHX_CTLG_TXT_P"]) > 0) {
         $ar_result["UF_PHX_CTLG_TXT_P_ENUM"] = CUserFieldEnum::GetList(array(), array(
-            "ID" => $ar_result["UF_PHX_CTLG_TXT_P"],
-        ))->GetNext();
+                    "ID" => $ar_result["UF_PHX_CTLG_TXT_P"],
+                ))->GetNext();
     }
 
-    if(strlen($ar_result["UF_PHX_CTLG_TXT_P_ENUM"]["XML_ID"]) <= 0)
+    if (strlen($ar_result["UF_PHX_CTLG_TXT_P_ENUM"]["XML_ID"]) <= 0)
         $ar_result["UF_PHX_CTLG_TXT_P_ENUM"]["XML_ID"] = "short";
 
     $arResult = array_merge($arResult, $ar_result);
@@ -39,96 +43,96 @@ if(intval($arResult["ID"]))
 
 $arResult["VIEW"] = $arParams["VIEW"];
 
-if(!strlen($arParams["VIEW"]))
+if (!strlen($arParams["VIEW"]))
     $arResult["VIEW"] = "FLAT";
 
+foreach ($arResult["ITEMS"] as $key => $arItem) {
+    $arResult["SOSTOYANIE_TOVARA"][$arItem["ID"]] = $arItem["PROPERTIES"]["SOSTOYANIE_TOVARA"]["VALUE"];
+    $arResult["DETAIL_TEXT"][$arItem["ID"]] = $arItem["DETAIL_TEXT"];
+}
 
 $arResult["rating"] = array();
 
-if(!empty($arResult["ITEMS"]) && $PHOENIX_TEMPLATE_ARRAY["ITEMS"]["RATING"]["ITEMS"]["USE_VOTE"]["VALUE"]["ACTIVE"] == "Y" && $PHOENIX_TEMPLATE_ARRAY["ITEMS"]["CATALOG_ITEM_FIELDS"]["ITEMS"]["PROPS_IN_LIST_FOR_".$arResult["VIEW"]]["VALUE"]["RATE_VOTE"] == "Y")
-{
-    if($PHOENIX_TEMPLATE_ARRAY["ITEMS"]["RATING"]["ITEMS"]["USE_REVIEW"]["VALUE"]["ACTIVE"] == "Y")
-    {
+if (!empty($arResult["ITEMS"]) && $PHOENIX_TEMPLATE_ARRAY["ITEMS"]["RATING"]["ITEMS"]["USE_VOTE"]["VALUE"]["ACTIVE"] == "Y" && $PHOENIX_TEMPLATE_ARRAY["ITEMS"]["CATALOG_ITEM_FIELDS"]["ITEMS"]["PROPS_IN_LIST_FOR_" . $arResult["VIEW"]]["VALUE"]["RATE_VOTE"] == "Y") {
+    if ($PHOENIX_TEMPLATE_ARRAY["ITEMS"]["RATING"]["ITEMS"]["USE_REVIEW"]["VALUE"]["ACTIVE"] == "Y") {
         $arResult["RATING_VIEW"] = "full";
         $arResult["ITEMS_ID"] = array();
-        foreach ($arResult["ITEMS"] as $key => $arItem){
-           $arResult["ITEMS_ID"][] = $arItem["ID"];
+        foreach ($arResult["ITEMS"] as $key => $arItem) {
+            $arResult["ITEMS_ID"][] = $arItem["ID"];
         }
-    }
-    else
-    {
+    } else {
         $arResult["RATING_VIEW"] = "simple";
 
-        foreach ($arResult["ITEMS"] as $key => $arItem){
-           $arResult["rating"][$arItem["ID"]] = (strlen($arItem["PROPERTIES"]["rating"]["VALUE"]))?round($arItem["PROPERTIES"]["rating"]["VALUE"]):"0";
+        foreach ($arResult["ITEMS"] as $key => $arItem) {
+            $arResult["rating"][$arItem["ID"]] = (strlen($arItem["PROPERTIES"]["rating"]["VALUE"])) ? round($arItem["PROPERTIES"]["rating"]["VALUE"]) : "0";
         }
     }
 }
 $arEditAreaId = array();
 
-if(!empty($arResult["ITEMS"]))
-{
-    foreach ($arResult["ITEMS"] as $key => $arItem){
-        $arEditAreaId[$arItem["ID"]] = $this->GetEditAreaId($arItem['ID']).'_list';
+if (!empty($arResult["ITEMS"])) {
+    foreach ($arResult["ITEMS"] as $key => $arItem) {
+        $arEditAreaId[$arItem["ID"]] = $this->GetEditAreaId($arItem['ID']) . '_list';
     }
 }
 
 CPhoenix::SetResultModifierCatalogElements($arResult, $arParams, $arEditAreaId);
 
-
+//echo "<pre>"; print_r($arResult["ITEMS"][0]); echo "</pre>";
 
 $cp = $this->__component;
- 
-if (is_object($cp))
-{
+
+if (is_object($cp)) {
     $cp->arResult['UF_PHX_CTLG_TITLE'] = $ar_result["UF_PHX_CTLG_TITLE"];
     $cp->arResult['UF_PHX_CTLG_KWORD'] = $ar_result["UF_PHX_CTLG_KWORD"];
     $cp->arResult['UF_PHX_CTLG_DSCR'] = $ar_result["UF_PHX_CTLG_DSCR"];
     $cp->arResult['UF_PHX_CTLG_H1'] = $ar_result["UF_PHX_CTLG_H1"];
-    
-    $cp->SetResultCacheKeys(array('ITEMS_ID','rating', 'UF_PHX_CTLG_TITLE', 'UF_PHX_CTLG_KWORD', 'UF_PHX_CTLG_DSCR', 'UF_PHX_CTLG_H1', 'ITEMS'));
-    
+
+    $cp->SetResultCacheKeys(array('ITEMS_ID', 'rating', 'UF_PHX_CTLG_TITLE', 'UF_PHX_CTLG_KWORD', 'UF_PHX_CTLG_DSCR', 'UF_PHX_CTLG_H1', 'ITEMS'));
+
     $arResult['UF_PHX_CTLG_TITLE'] = $cp->arResult['UF_PHX_CTLG_TITLE'];
     $arResult['UF_PHX_CTLG_KWORD'] = $cp->arResult['UF_PHX_CTLG_KWORD'];
     $arResult['UF_PHX_CTLG_DSCR'] = $cp->arResult['UF_PHX_CTLG_DSCR'];
     $arResult['UF_PHX_CTLG_H1'] = $cp->arResult['UF_PHX_CTLG_H1'];
 }
 
-
-
-
-
-
 $this->SetViewTarget('available-show');
-    echo (CPhoenix::IssetOffers($arResult)) ? "d-none" : "";
+echo (CPhoenix::IssetOffers($arResult)) ? "d-none" : "";
 $this->EndViewTarget();
 
-
-if(isset($arParams["SEARCH_ELEMENTS_ID"]))
-{
-    if(!empty($arParams["SEARCH_ELEMENTS_ID"]))
-    {
+if (isset($arParams["SEARCH_ELEMENTS_ID"])) {
+    if (!empty($arParams["SEARCH_ELEMENTS_ID"])) {
 
         foreach ($arParams["SEARCH_ELEMENTS_ID"] as $key => $value) {
-           $arParams["SEARCH_ELEMENTS_ID"][$key] = intval($value);
+            $arParams["SEARCH_ELEMENTS_ID"][$key] = intval($value);
         }
 
 
         $newArResult = array();
 
-        if(!empty($arResult["ITEMS"]))
-        {
-            foreach ($arResult["ITEMS"] as $key => $value)
-            {
+        if (!empty($arResult["ITEMS"])) {
+            foreach ($arResult["ITEMS"] as $key => $value) {
                 $newKey = array_search(intval($value["ID"]), $arParams["SEARCH_ELEMENTS_ID"]);
 
                 $newArResult[$newKey] = $value;
             }
         }
-        
-        ksort($newArResult);
+
+
+
+//        ksort($newArResult);
 
         $arResult["ITEMS"] = array_values($newArResult);
-
     }
 }
+
+$array_price = [];
+$array_quantity = [];
+foreach ($arResult["ITEMS"] as $key => $row) {
+    $array_price[$key] = $row['PRICE']['PRICE'];
+    $array_quantity[$key] = $row['QUANTITY']['QUANTITY_VALUE'];
+}
+
+array_multisort($array_quantity, SORT_DESC, $array_price, SORT_DESC, $arResult["ITEMS"]);
+
+
