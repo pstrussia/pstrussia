ca1af8c5cb50c63544cce67ebeae0adeb5abd8e8
gvozdevdanil <gvozdev-danil@mail.ru>
2022-11-22 18:48:35 +0300

    change default sort - по популярности

--- local/templates/divasoft/components/bitrix/catalog/catalog/section.php
+++ local/templates/divasoft/components/bitrix/catalog/catalog/section.php
@@ -961,7 +961,7 @@ if (strlen($pictureInHeadIsset)) {
 										false
 									);}
                                     if ($sort2 == 'SORT'):
-                                        $sort2 = 'CATALOG_QUANTITY';
+                                        $sort2 = 'PROPERTY_EXPENSE';
                                         $sort_order2 = 'desc';
 
                                     endif;
