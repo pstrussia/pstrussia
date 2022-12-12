c459753706771a0e28f7a34c8d11fa7f78cbfd33
gvozdevdanil <gvozdev-danil@mail.ru>
2022-12-13 02:22:08 +0300

    dev-16255 Добавление возможности автоподставления номера телефона в маску, если копируется и вставляется полностью номер телефона

--- local/templates/divasoft/js/forms.js
+++ local/templates/divasoft/js/forms.js
@@ -481,7 +481,24 @@ $(document).on("click", ".btn-submit", function ()
 
 });
 
-  
+
+
+
+$(document).on("paste", "input[type='tel'],input[autocomplete='tel']", function(e) {
+  var clipboardData = e.originalEvent.clipboardData || window.clipboardData;
+  var pastedData = clipboardData.getData("text");
+// Проверяем первый символ на соответствие "+7" или "8"
+if (pastedData[0] === "+" || pastedData[0] === "8") {
+  // Если символ соответствует, убираем его
+  var formattedPhoneNumber = pastedData.substring(1);
+} else {
+  // Если первый символ не соответствует, оставляем номер без изменений
+  var formattedPhoneNumber = pastedData;
+}
+$(this).val(formattedPhoneNumber);
+});
+
+
 $(document).on("change", "input[type='tel'],input[autocomplete='tel']", function () {
   var val = $(this).val();
   if (val.replace(/\D/g, '').startsWith('78')) {
